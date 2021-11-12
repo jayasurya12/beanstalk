d78fcfcc190e62b162bd7170327a253b04796b1b
jayasurya12 <jayasurya12vj@gmail.com>
2021-11-12 22:26:02 +0530

    first

---
+++ server.js
@@ -0,0 +1,14 @@
+const express=require('express')
+const app=express();
+app.use(express.json())
+app.get("/",(req,res)=>{
+    res.send('<h2>Hello Server is Working</h2>')
+})
+app.get("/new",(req,res)=>{
+    res.send("<h3>Hellow new route is working</h3>")
+})
+
+app.listen(5000,()=>{
+    console.log('server is started');
+})
+module.exports=app
